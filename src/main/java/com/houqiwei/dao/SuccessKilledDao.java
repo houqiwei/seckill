6819babaa65c480c273de84e88429f0e6aa8fb38
houqiwei <qiwei_hou@163.com>
2019-05-16 16:30:16 +0800

    实体类及dao接口

---
+++ src/main/java/com/houqiwei/dao/SuccessKilledDao.java
@@ -0,0 +1,21 @@
+package com.houqiwei.dao;
+
+import com.houqiwei.model.SuccessKilled;
+
+public interface SuccessKilledDao {
+
+    /**
+     * 插入购买明细，可过滤重复
+     * @param seckillId
+     * @param userPhone
+     * @return
+     */
+    int insertSuccessKilled(long seckillId, long userPhone);
+
+    /**
+     * 根据id查询SuccessKilled并携带秒杀产品对象实体
+     * @param seckillId
+     * @return
+     */
+    SuccessKilled queryByIdWithSeckill(long seckillId);
+}
